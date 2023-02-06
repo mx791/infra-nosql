e2ac3051ca3ef500732a097a0c2c24a3c6e3c5f0
mx791 <56536939+mx791@users.noreply.github.com>
2023-02-06 14:14:54 +0100

    Update zone_2_zone_estimator.py

--- etl/zone_2_zone_estimator.py
+++ etl/zone_2_zone_estimator.py
@@ -4,8 +4,8 @@ myclient = pymongo.MongoClient("mongodb://192.168.3.160:27019/")
 mydb = myclient["projet_nosql"]
 mytable = mydb["taxis"]
 
-start_area = input("pickup area:")
-destination_area =  input("dropoff area:")
+start_area = int(input("pickup area:"))
+destination_area =  int(input("dropoff area:"))
 
 myquery = [
     {"$match": {"Pickup Community Area": start_area, "Dropoff Community Area": destination_area}},
