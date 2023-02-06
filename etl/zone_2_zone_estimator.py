b2d1a824fa7ca6fc2d1d3638da77cb8c3bd44e5c
mx791 <56536939+mx791@users.noreply.github.com>
2023-02-06 13:49:54 +0100

    Update zone_2_zone_estimator.py

--- etl/zone_2_zone_estimator.py
+++ etl/zone_2_zone_estimator.py
@@ -4,9 +4,11 @@ myclient = pymongo.MongoClient("mongodb://192.168.3.160:27019/")
 mydb = myclient["projet_nosql"]
 mytable = mydb["taxis"]
 
+start_area = 5
+destination_area = 6
 
 myquery = [
-    {"$match": {"Pickup Community Area": 5, "Dropoff Community Area": 6}},
+    {"$match": {"Pickup Community Area": start_area, "Dropoff Community Area": destination_area}},
     {"$group": {
         "_id": "$Pickup Hour",
         "avg_seconds": {"$avg": "$Trip Seconds"},
