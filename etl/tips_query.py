78d593d885cfdd8e6a2698035773df9ff32810df
antoinemontagne <72302418+antoinemontagne@users.noreply.github.com>
2023-02-07 17:46:22 +0100

    Add files via upload

--- etl/tips_query.py
+++ etl/tips_query.py
@@ -1,5 +1,6 @@
 import pymongo
 import time
+import pandas as pd
 
 myclient = pymongo.MongoClient("mongodb://192.168.3.160:27019/")
 mydb = myclient["projet_nosql"]
@@ -13,6 +14,9 @@ myquery = [
         "nb": {"$sum": 1}}
     }
 ]
+
+chemin = '/home/ubuntu/infra-nosql/etl'
+df = pd.read_csv(chemin + '/community_areas', delimiter=',', usecols=["No.", "Name"])
 
 print("\nRecherche en cours...")
 
@@ -43,10 +47,11 @@ for doc in results:
         total_tips += doc["avg_tips"]
         nb_community_area += 1
     
-            
+min_name = df[df['No.']==int(min_tips_h)]["Name"].values[0]
+max_name = df[df['No.']==int(max_tips_h)]["Name"].values[0]
 print("Comparaison de ", nb, "trajet")
-print(f"Dropoff Community Area {min_tips_h} avec la plus faible moyenne de tips : {min_tips}")
+print(f"Dropoff Community Area {min_tips_h}:{min_name} avec la plus faible moyenne de tips : {min_tips}")
 print(f"Moyenne tips sur toutes les Community Areas {total_tips/nb_community_area}")
-print(f"Dropoff Community Area {max_tips_h} avec la plus forte moyenne de tips : {max_tips}\n")
+print(f"Dropoff Community Area {max_tips_h}:{max_name} avec la plus forte moyenne de tips : {max_tips}\n")
     
 print("Execution time : ", sum(execution_times)/nb_execution, "\n")
