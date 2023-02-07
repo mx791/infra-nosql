16db52d8e9c3217edace0b1a5cd0ee1d1a950336
antoinemontagne <72302418+antoinemontagne@users.noreply.github.com>
2023-02-07 12:02:12 +0100

    adding tips_query.py

---
+++ etl/tips_query.py
@@ -0,0 +1,44 @@
+import pymongo
+import time
+
+myclient = pymongo.MongoClient("mongodb://192.168.3.160:27019/")
+mydb = myclient["projet_nosql"]
+mytable = mydb["taxis"]
+
+myquery = [
+    {$group: {
+        _id: "$Dropoff Community Area",
+        avg_tips: {$avg: "$Tips"},
+        avg_distance: {$avg: "$Trip Miles"},
+        nb: {$sum: 1}}
+    }
+]
+
+print("\nRecherche en cours...")
+start_time = time.time()
+
+results = list(mytable.aggregate(myquery))
+
+end_time = time.time()
+
+min_tips_h, min_tips = 0, 100000
+max_tips_h, max_tips = 0, 0
+
+nb = 0
+
+for doc in results:
+    if doc["_id"] != None:
+        if min_tips >= doc["avg_tips"]:
+            min_tips_h = doc["_id"]
+            min_tips = doc["avg_tips"]
+        elif max_tips >= doc["avg_tips"]:
+            max_tips_h = doc["_id"]
+            max_tips = doc["avg_tips"]
+        nb += doc["nb"]
+    
+            
+print("Comparaison de ", nb, "trajet\n")
+print(f"Dropoff Community Area {min_tips_h} avec la plus faible moyenne de tips : {min_tips}\n")
+print(f"Dropoff Community Area {max_tips_h} avec la plus forte moyenne  de tips : {max_tips}\n")
+    
+print("Execution time : ", time.time() - start_time, "\n")
