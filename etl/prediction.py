f38462b837994d84e4f92ab01ab121f34e46261a
PierreAntoineClouzeau01 <113109585+PierreAntoineClouzeau01@users.noreply.github.com>
2023-02-08 11:44:57 +0100

    Update prediction.py

--- etl/prediction.py
+++ etl/prediction.py
@@ -29,7 +29,7 @@ start = time.time()
 
 print("\nRecherche en cours...")
 
-results = pd.DataFrame(mytable.find({ "Trip Total" : "true"}))
+results = pd.DataFrame(list(mytable.find({ "Trip Total" : "true"})))
 end = time.time()
 print("La query a mis {}s à s'exécuter".format(np.round(end-start)))
 
